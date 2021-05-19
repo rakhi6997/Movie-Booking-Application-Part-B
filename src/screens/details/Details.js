ba4fd28ddad1b2881d3514af86df695fadfcd891
rakhi <ramakrishna060997@gmail.com>
2021-05-19 23:32:52 +0530

    Checkpoint 4:Integration With the Backend

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -46,10 +46,11 @@ class Details extends Component {
     componentWillMount() {
         let currentState = this.state;
         currentState.movie = moviesData.filter((mov) => {
-            return mov.id === this.props.match.params.id
+            return ( mov.title) == this.props.match.params.id
         })[0];
 
         this.setState({ currentState });
+        console.log(""+this.props.match.params.id)
     }
 
     artistClickHandler = (url) => {
